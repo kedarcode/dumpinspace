d9d16b607a8c1f98bf0369d298f1e7bcc668bf77
kedar shelar <98396591+kedarcode@users.noreply.github.com>
2022-09-22 14:31:21 +0530

    first commit

---
+++ sqldumpauto/dumpautomation.py
@@ -0,0 +1,74 @@
+import os, os.path, time, sys
+import boto3
+from datetime import datetime
+import shutil
+
+sqlcred = {
+    'HOST': '127.0.0.1',
+    'PORT': '3307',
+    'DB_USER': 'root',
+    'DB_PASS': '',
+    'database': 'pcbudge'}
+
+mongocred = {
+    'HOST': '127.0.0.1',
+    'PORT': '27017',
+    'username': '',
+    'password': '',
+    'database': 'PCBUDGE'
+}
+
+session = boto3.session.Session()
+client = session.client('s3',
+                        endpoint_url='https://onepercentstartups-001.sgp1.digitaloceanspaces.com/',
+                        region_name='sgp1',
+                        aws_access_key_id=' DO00YTW42UNAGUETY9L9',
+                        aws_secret_access_key='1ysV9psqtOXGd9DNguezV6JwrnW0w8NpPvJGnCwPGf8')
+
+
+def create_folder_backup():
+    dt = datetime.now()
+    directory = ('backups/bk_%s-%s-%s__%s_%s_%s' % (dt.month, dt.day, dt.year, dt.hour, dt.minute, dt.second))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return directory
+
+
+def get_dump(database, dir):
+    filestamp = str(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+    os.popen("mysqldump -h %s -P %s -u %s -p%s %s > %s.sql" % (
+        sqlcred['HOST'], sqlcred['PORT'], sqlcred['DB_USER'], sqlcred['DB_PASS'], database[0], dir + "/" +
+        database[0] + "_" + filestamp))
+
+
+def get_mongo(database, collection=None, folder=None):
+    filestamp = str(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+
+    if collection:
+        command = f'mongodump --host={mongocred["HOST"]} --db={database} '\
+                  f'--collection={collection} --username={mongocred["username"]}' +\
+                  f' --password={mongocred["password"]} --gzip  --out={folder + "/" + database + "_" + filestamp}'
+        os.popen(command)
+    else:
+        command = f'mongodump --host={mongocred["HOST"]} --db={database} '\
+                  f' --username={mongocred["username"]}'\
+                  f' --password={mongocred["password"]} --gzip  --out={folder + "/" + database + "_" + filestamp}'
+        os.popen(command)
+    print(command)
+
+
+if __name__ == "__main__":
+    dir = create_folder_backup()
+
+    get_dump(sqlcred['database'], dir)
+
+    if len(sys.argv) > 1:
+        get_mongo(mongocred['database'], sys.argv[1], folder=dir)
+    else:
+        get_mongo(mongocred['database'], folder=dir)
+    shutil.make_archive(dir, 'zip', dir)
+
+    client.put_object(Bucket='dump',
+                      Key=f'backups/{dir}.zip',
+                      ACL='private',
+                      )
